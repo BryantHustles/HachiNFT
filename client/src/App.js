b53b091b499d3cdd85b935ebd6e46a1d3a48f8d3
BryantHustles <104115116+BryantHustles@users.noreply.github.com>
2022-07-04 14:12:20 -0400

    Working on react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,45 @@
 import React, { Component } from "react";
-import HSTLEToken from "./contracts/HSTLEToken.json";
-import MyTokenSale from "./contracts/MyTokenSale.json"
-import KycContract from "./contracts/KycContract.json"
+import Token from "./contracts/HACHINFT.json";
+import Wallet from "./contracts/HachiWallet.json"
+import Whitelist from "./contracts/HachiWhitelist.json"
 import getWeb3 from "./getWeb3";
 
 import "./App.css";
 
 class App extends Component {
-  state = { loaded:false, kycAddress: "0x123", tokensToPurchase: 0, tokenSaleAddress: null, userTokens: 0, totalSupply: 0};
+  state = { loaded:false};//, kycAddress: "0x123", tokensToPurchase: 0, tokenSaleAddress: null, userTokens: 0, totalSupply: 0};
 
   componentDidMount = async () => {
     try {
       // Get network provider and web3 instance.
       this.web3 = await getWeb3();
+      this.networkId = await this.web3.eth.net.getId();
 
       // Use web3 to get the user's accounts.
        this.accounts = await this.web3.eth.getAccounts();
 
       // Get the contract instance.
-      this.networkId = await this.web3.eth.net.getId();
       this.tokenInstance = new this.web3.eth.Contract(
-        HSTLEToken.abi,
-        HSTLEToken.networks[this.networkId] && HSTLEToken.networks[this.networkId].address,
+        Token.abi,
+        Token.networks[this.networkId] && Token.networks[this.networkId].address,
       );
 
-      this.networkId = await this.web3.eth.net.getId();
-      this.tokenSaleInstance = new this.web3.eth.Contract(
-        MyTokenSale.abi,
-        MyTokenSale.networks[this.networkId] && MyTokenSale.networks[this.networkId].address,
+      this.walletInstance = new this.web3.eth.Contract(
+        Wallet.abi,
+        Wallet.networks[this.networkId] && Wallet.networks[this.networkId].address,
       );
 
-      this.networkId = await this.web3.eth.net.getId();
-      this.kycContractInstance = new this.web3.eth.Contract(
-        KycContract.abi,
-        KycContract.networks[this.networkId] && KycContract.networks[this.networkId].address,
+      this.whitelistInstance = new this.web3.eth.Contract(
+        Whitelist.abi,
+        Whitelist.networks[this.networkId] && Whitelist.networks[this.networkId].address,
       );
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.listenToTokenTransfer()
-      this.setState({loaded:true, kycAddress: "0x123", tokensToPurchase: 0, tokenSaleAddress: MyTokenSale.networks[this.networkId].address}, this.updateUserTokens);
+
+      //Example listener
+      //this.listenToTokenTransfer()
+      this.setState({loaded:true}); //, kycAddress: "0x123", tokensToPurchase: 0, tokenSaleAddress: MyTokenSale.networks[this.networkId].address}, this.updateUserTokens);
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -48,24 +48,24 @@ class App extends Component {
       console.error(error);
     }
 
-    this.updateTotalSupply()
+    //this.updateTotalSupply()
   };
 
 
-  updateUserTokens = async() => {
-    let userTokens = await this.tokenInstance.methods.balanceOf(this.accounts[0]).call();
-    this.setState({userTokens:userTokens});
-  }
+  // updateUserTokens = async() => {
+  //   let userTokens = await this.tokenInstance.methods.balanceOf(this.accounts[0]).call();
+  //   this.setState({userTokens:userTokens});
+  // }
 
-  updateTotalSupply = async() => {
-    let totalSupply = await this.tokenInstance.methods.totalSupply().call();
-    this.setState({totalSupply:totalSupply});
-  }
+  // updateTotalSupply = async() => {
+  //   let totalSupply = await this.tokenInstance.methods.totalSupply().call();
+  //   this.setState({totalSupply:totalSupply});
+  // }
 
-  listenToTokenTransfer = async() => {
-    this.tokenInstance.events.Transfer({to: this.accounts[0]}).on("data",this.updateUserTokens)
-    this.tokenInstance.events.Transfer({to: this.accounts[0]}).on("data",this.updateTotalSupply)
-  }
+  // listenToTokenTransfer = async() => {
+  //   this.tokenInstance.events.Transfer({to: this.accounts[0]}).on("data",this.updateUserTokens)
+  //   this.tokenInstance.events.Transfer({to: this.accounts[0]}).on("data",this.updateTotalSupply)
+  // }
 
   handleInputChange = (event) => {
     const target = event.target;
@@ -76,15 +76,15 @@ class App extends Component {
     });
   }
 
-  handleKycWhitelisting = async () => {
-      await this.kycContractInstance.methods.setKycCompleted(this.state.kycAddress).send({from: this.accounts[0]});
-      alert("KYC for " + this.state.kycAddress+ " is completed")
-  }
+//   handleMintHachi = async () => {
+//       await this.kycContractInstance.methods.setKycCompleted(this.state.mintQuantity).send({from: this.accounts[0]});
+//       alert("KYC for " + this.state.mintQuantity+ " is completed")
+//   }
 
-  handlePurchaseTokens = async () => {
-    //await this.web3.eth.sendTransaction({from: this.accounts[0], to: this.state.tokenSaleAddress, value: this.web3.utils.toWei(this.state.tokensToPurchase,"wei"), gas: "1500000"});
-    await this.tokenSaleInstance.methods.buyTokens(this.accounts[0]).send({from: this.accounts[0], value: this.web3.utils.toWei(this.state.tokensToPurchase,"wei")})
-}
+//   handlePurchaseTokens = async () => {
+//     //await this.web3.eth.sendTransaction({from: this.accounts[0], to: this.state.tokenSaleAddress, value: this.web3.utils.toWei(this.state.tokensToPurchase,"wei"), gas: "1500000"});
+//     await this.tokenSaleInstance.methods.buyTokens(this.accounts[0]).send({from: this.accounts[0], value: this.web3.utils.toWei(this.state.tokensToPurchase,"wei")})
+// }
 
   render() {
     if (!this.state.loaded) {
@@ -92,27 +92,32 @@ class App extends Component {
     }
     return (
       <div className="App">
+
         <div>
-          <h1>$HUSTLE Token Sale Page Page</h1>
-          <p> Get your Tokens today!</p>
+          <h1>Hachi NFT Home Page</h1>
         </div>
-        <div>
-          <h2>Complete the KYC Below</h2>
-          Address to Allow : <input type="text" placeholder="Wallet Address 0x123..." name="kycAddress" value={this.state.kycAddress} onChange={this.handleInputChange}></input>
-          <button onClick={this.handleKycWhitelisting}>Submit</button>
-        </div>
-        <div>
-          <h2>Buy your HSTLE Token</h2>
-          <h3>Tokens can also be purchased by sending Eth to the following contract address:</h3>
-          <p>{this.state.tokenSaleAddress}</p>
-          <br></br>
-          Tokens to purchase : <input type="number" placeholder="Ex. 100" name="tokensToPurchase" value={this.state.tokensToPurchase} onChange={this.handleInputChange}></input>
-          <button onClick={this.handlePurchaseTokens}>Purchase</button>
-          <p>You currently have {this.state.userTokens} Hustle Tokens</p>
-        </div>
-        <div>
-          <h2>Current Total Supply of Tokens</h2>
-          <p>{this.state.totalSupply}</p>
+
+        <div style={{ display: "grid", gridTemplateColumns: "repeat(3, 1fr)", gridGap: 10 }}>      
+          <div>
+            <h2>Mint Your Hachi</h2>
+            Amount to Mint : <input type="number" min="1" max="3" placeholder="1" name="mintQuantity" value={this.state.mintQuantity} onChange={this.handleInputChange}></input>
+            <button onClick={this.handleMintHachi}>Mint Hachi</button>
+          </div>
+
+          <div>
+            <h2>Hachi Dashboard</h2>
+          </div>
+
+          <div>
+            <h2>Administrative Tools</h2>
+              <h3>Mint</h3>
+                <h4>Current Info Info</h4>
+                <h4>Update Mint Info</h4>
+              <h3>Whitelist</h3>
+              <h3>Payment</h3>
+                <h4>Update Payment Info</h4>
+                <h4>Request Payment</h4>
+          </div>
         </div>
       </div>
     );
